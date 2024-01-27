8368a53aaa9aff040af40a274bb4344a58241ff5
f-fardin-dev <fakurian.fardin@gmail.com>
2024-01-27 23:41:14 +0330

    feat: add delete task functionality in the TaskItem

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,13 +1,29 @@
 import { Task } from "@app/types/task";
 import { DeleteIcon } from "./Icons";
+import { deleteTask } from "@app/Services/connections";
+import { useState } from "react";
 
 interface TaskItemProps extends Task {}
 
-export const TaskItem = ({ title, waitingToBeAdded }: TaskItemProps) => {
+export const TaskItem = ({ id, title, waitingToBeAdded }: TaskItemProps) => {
+  const [deleteLoading, setDeleteLoading] = useState(false);
+
+  const handleDeleteTask = async () => {
+    setDeleteLoading(true);
+    try {
+      const result = await deleteTask(id);
+      if (!result) {
+        setDeleteLoading(false);
+      }
+    } catch (err) {
+      setDeleteLoading(false);
+    }
+  };
+
   return (
     <div className="bg-violet-500 p-4 rounded-md flex justify-between relative overflow-hidden">
-      {title.at(0)?.toUpperCase() + title.slice(1)}
-      <span>
+      {title?.at(0)?.toUpperCase() + title?.slice(1)}
+      <span onClick={handleDeleteTask}>
         <DeleteIcon />
       </span>
       {waitingToBeAdded && (
@@ -15,7 +31,7 @@ export const TaskItem = ({ title, waitingToBeAdded }: TaskItemProps) => {
           <span>Waiting To Be Added!</span>
         </div>
       )}
-      {false && (
+      {deleteLoading && (
         <>
           <div className="linear-loader bg-violet-500 rounded absolute right-2 left-2 bottom-[2px]"></div>
           <div className="absolute bg-slate-300 opacity-40 inset-0"></div>
